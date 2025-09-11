1610ff59905f92ab22463629002ca6632db2e5ff
Sabers2610 <luissaez261002@gmail.com>
2025-09-11 13:31:32 -0300

    Agregada modelo de Pais, Region, Comuna

--- Fase 2/Evidencias Proyecto/Evidencias de sistema/backend/app/models/ficha_jugador.py
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema/backend/app/models/ficha_jugador.py
@@ -3,6 +3,10 @@ from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.orm import relationship
 from datetime import date
 from app.db import Base
+from sqlalchemy import ForeignKey
+from .pais import Pais
+from .estadisticas_jugador import EstadisticasJugador
+from .evaluacion_fisica import EvaluacionFisica
 
 class FichaJugador(Base):
     __tablename__ = "FICHA_JUGADOR"
@@ -14,18 +18,19 @@ class FichaJugador(Base):
     segundo_apellido: Mapped[str] = mapped_column(String(12), nullable=True)
     enfermedades_cronicas: Mapped[str] = mapped_column(String(500), nullable=True)
     fecha_nacimiento: Mapped[date] = mapped_column(Date, nullable=False)
-    nacionalidad: Mapped[str] = mapped_column(String(30), nullable=False)
+    nacionalidad: Mapped[int] = mapped_column(Integer, ForeignKey("PAIS.id_pais"), nullable=False)  # FK a PAIS
     correo_electronico: Mapped[str] = mapped_column(String(320), nullable=False, unique=True)
     pierna_habil: Mapped[int] = mapped_column(Integer, nullable=True)  # 1: derecha, 2: izquierda, 3: ambas
     genero: Mapped[bool] = mapped_column(Boolean, nullable=False)  # True: masculino, False: femenino
 
     # Relaciones
-    # estadisticas_jugador: Mapped[list["Estadistica_jugador"]] = relationship(
-    #     "ESTADISTICA_JUGADOR", back_populates="ficha_jugador", cascade="all, delete-orphan"
-    # )
-    # evaluaciones_fisicas: Mapped[list["Evaluacion_fisica"]] = relationship(
-    #     "EVALUACION_FISICA", back_populates="ficha_jugador", cascade="all, delete-orphan"
-    # )
+    estadisticas_jugador: Mapped[list["EstadisticasJugador"]] = relationship(
+        EstadisticasJugador, back_populates="ficha_jugador", cascade="all, delete-orphan"
+    )
+    evaluaciones_fisicas: Mapped[list["EvaluacionFisica"]] = relationship(
+        EvaluacionFisica, back_populates="ficha_jugador", cascade="all, delete-orphan"
+    )
+    pais: Mapped["Pais"] = relationship(Pais, back_populates="jugadores", cascade="all, delete-orphan")
     # lesiones: Mapped[list["Lesion"]] = relationship(
     #     "LESION", back_populates="ficha_jugador", cascade="all    , delete-orphan"
     # )
---
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema/backend/app/models/pais.py
@@ -0,0 +1,16 @@
+from sqlalchemy import String, Integer
+from sqlalchemy.orm import Mapped, mapped_column
+from app.db import Base
+from sqlalchemy.orm import relationship
+from .ficha_jugador import FichaJugador
+
+class Pais(Base):
+    __tablename__ = "PAIS"
+
+    id_pais: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    nombre_pais: Mapped[str] = mapped_column(String(100), nullable=False)
+
+    # Relaciones 
+    jugadores: Mapped[list[FichaJugador]] = relationship(
+        "FichaJugador", back_populates="pais", cascade="all, delete-orphan"
+    )
