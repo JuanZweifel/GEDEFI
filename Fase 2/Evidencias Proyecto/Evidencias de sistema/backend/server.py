d664f4f475fb1059f3266d53aaf20efa76e22ea7
JuanZweifel <jua.borquez@duocuc.cl>
2025-09-28 15:28:04 -0300

    Añade script de arranque para servidor ASGI

---
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema/backend/server.py
@@ -0,0 +1,10 @@
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "app.main:app",
+        # host=settings.SERVER_HOST,
+        # port=settings.SERVER_PORT,
+        # reload=settings.ENV in ["test", "dev"],
+        # log_level="debug" if settings.ENV in ["test", "dev"] else None,
+    )
