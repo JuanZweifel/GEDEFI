a0b9b7d2d64c652abbdfc3ab53e7e2268133f140
JuanZweifel <jua.borquez@duocuc.cl>
2025-11-25 20:36:05 -0300

    Cambio del script para iniciar el server
    Ahora permite un parámetro para seleccionar la base de datos objetivo

--- Fase 2/Evidencias Proyecto/Evidencias de sistema/Aplicación/backend/app/config.py
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema/Aplicación/backend/app/config.py
@@ -2,18 +2,15 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    POSTGRES_USER: str
-    POSTGRES_PASSWORD: str
-    POSTGRES_SERVER: str = "localhost"
-    POSTGRES_PORT: int = 5432
-    POSTGRES_DB: str
+    DB_TARGET: str = "local"
+    LOCAL_DB_URL: str
+    AIVEN_DB_URL: str
 
     @property
-    def database_url(self) -> str:
-        return (
-            f"postgresql+psycopg2://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}"
-            f"@{self.POSTGRES_SERVER}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
-        )
+    def database_url(self):
+        if self.DB_TARGET == "aiven":
+            return self.AIVEN_DB_URL
+        return self.LOCAL_DB_URL
 
     class Config:
         env_file = ".env"
--- Fase 2/Evidencias Proyecto/Evidencias de sistema/Aplicación/backend/server.py
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema/Aplicación/backend/server.py
@@ -1,11 +1,20 @@
 import uvicorn
+import argparse
+import os
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--db",
+        choices=["local", "aiven"],
+        default="local",
+        help="Selecciona la base de datos a utilizar (local o aiven).",
+    )
+    args = parser.parse_args()
+
+    os.environ["DB_TARGET"] = args.db
+
     uvicorn.run(
         "app.main:app",
         reload=True,
-        # host=settings.SERVER_HOST,
-        # port=settings.SERVER_PORT,
-        # reload=settings.ENV in ["test", "dev"],
-        # log_level="debug" if settings.ENV in ["test", "dev"] else None,
     )
